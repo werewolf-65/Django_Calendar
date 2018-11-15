766ac3dceb1af1637b685d0ad909650c2279ff3e
werewolf-65 <saileshsthnp@gmail.com>
2018-11-15 23:10:28 +0545

    added the authentication system

--- calendarproject/calendarproject/urls.py
+++ calendarproject/calendarproject/urls.py
@@ -14,15 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from myapp import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('login',auth_views.LoginView.as_view(),name="login"),
+    path('logout',auth_views.LogoutView.as_view(),{'next_page':'/'},name="logout"),
+    path('register',views.signup,name="signup"),
     path('entry/<int:pk>',views.details,name="details"),
     path('admin/', admin.site.urls),
     #for Add
     path('entry/add',views.add,name="add"),
     #for delete
-    path('entry/delete/<int:pk>',views.delete,name="delete")
+    path('entry/delete/<int:pk>',views.delete,name="delete"),
+    path('calendar',views.calendar,name="calendar")
 ]
--- calendarproject/myapp/views.py
+++ calendarproject/myapp/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login
 from .models import Entry
 from .forms import EntryForm
 
 # Create your views here.
 def index(request):
     #return HttpResponse('<h1>It Works!</h1>')
+    return render(request,'myapp/index.html')
+def calendar(request):
     entries=Entry.objects.all()
-    return render(request,'myapp/index.html',{'entries':entries})
+    return render(request,'myapp/calendar.html',{'entries':entries})
 def details(request,pk):
     entry=get_object_or_404(Entry,pk=pk)
     return render(request,'myapp/details.html',{'entry':entry})
@@ -32,3 +36,16 @@ def delete(request,pk):
         entry=get_object_or_404(Entry,pk=pk)
         entry.delete()
     return HttpResponseRedirect('/')
+def signup(request):
+    if request.method=="POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            return redirect('/calendar')
+    else:
+        form=UserCreationForm()
+    return render(request,'registration/signup.html',{'form':form})
